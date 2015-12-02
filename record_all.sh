8c3093dd560dabf959411ce72ea0833d998f7b2b
Matthew Switlik <switlik@oakland.edu>
2015-12-02 16:51:03 -0500

    -n and -t parameters and defaults

--- record_all.sh
+++ record_all.sh
@@ -2,17 +2,46 @@
 
 clear
 
-#avconv -f video4linux2 -b 230M -r 30 -s 640x480 -pix_fmt yuv422p -i /dev/video0 -vcodec libx264 -t 10 -y cam_0.avi 2>&1 &> cam_0.log &
-#avconv -f video4linux2 -b 230M -r 30 -s 640x480 -pix_fmt yuv422p -i /dev/video1 -vcodec libx264 -t 10 -y cam_1.avi 2>&1 &> cam_1.log &
+# Get arguments
+while [[ $# > 1 ]]
+do
+key="$1"
+case $key in
+    -n|--numcam)
+    NUMCAM="$2"
+    shift # past argument
+    ;;
+    -t|--time)
+    DURATION="$2"
+    shift # past argument
+    ;;
+    *)
+            # unknown option
+    ;;
+esac
+shift # past argument or value
+done
 
-avconv -f video4linux2 -input_format mjpeg -video_size 1280x720 -i /dev/video0 -c:v copy -t 15 -y cam_0.mp4 2>&1 &> cam_0.log &
 
-#Cut the output size in half with h264.
-#avconv -f video4linux2 -input_format mjpeg -video_size 1280x720 -i /dev/video0 -vcodec libx264 -preset ultrafast -threads 0 -t 15 -y cam_0.mp4 2>&1 &> cam_0.log &
+# Set default parameters if not set.
+NUMCAM=${NUMCAM-1}
+DURATION=${DURATION-20}
+
+# List the Cameras being used.
+echo -e "Number of Cameras: $NUMCAM"
+echo -e "Duration: $DURATION"
+((NUMCAM--))
+for i in $(seq 0 $NUMCAM)
+do
+	echo "/dev/video$i"
+	avconv -f video4linux2 -input_format mjpeg -video_size 1280x720 -i /dev/video$i -c:v copy -t $DURATION -y cam_$i.mp4 2>&1 &> cam_$i.log &
+
+done
+
+#avconv -f video4linux2 -input_format mjpeg -video_size 1280x720 -i /dev/video1 -c:v copy -t 60 -y cam_1.mp4 2>&1 &> cam_1.log &
+
+# Use h264 to shrink output filesize.
+#avconv -f video4linux2 -input_format mjpeg -video_size 1280x720 -i /dev/video0 -vcodec libx264 -preset ultrafast -threads 0 -t 60 -y cam_0.mp4 2>&1 &> cam_0.log &
 
 
-#Ryan's Suggestion:
-#avconv -f video4linux2 -input_format mjpeg -video_size 1280x720 -i /dev/video0 -c:v copy video0.mp4 > /dev/null 2>&1 < /dev/null &
-#avconv -f video4linux2 -input_format mjpeg -video_size 1280x720 -i /dev/video1 -c:v copy video1.mp4 > /dev/null 2>&1 < /dev/null &
-
-echo -e "\nRECORDINGS LAUNCHED.\n"
+echo -e "RECORDINGS LAUNCHED."
