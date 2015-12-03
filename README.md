321c9704b7a5353de790dc076936547a4171e230
SWiT <switlikm@gmail.com>
2015-12-02 21:56:25 -0500

    new resolution & starting camera options

--- README.md
+++ README.md
@@ -1,3 +1,23 @@
-# MultiCamRec
-This script is meant for launching multiple processes of avconv to record video from multiple USB webcams. It has been tested on Ubuntu 14.04.3 LTS.
+# MultiCamRec: Multiple Camera Recorder Bash Script
+This bash script is meant for launching multiple instances of avconv from the libav-tools package to record video from multiple USB webcams. The video sources must be consecutive (Ex. /dev/video1, /dev/video2, and /dev/video). Each video is saved as cam_#.mp3 with the terminal output of avconv saved as cam_#.log. This script has been tested on Ubuntu 14.04.3 LTS.
+
+Example: Record 3 webcams starting with /dev/video1 for 60 seconds
+./record.sh -s 1 -n 3 -t 60
+
+Usage:
+./record.sh [OPTIONS]
+
+Default Options:
+    startcam    0
+    numcam      1
+    time        15
+    codec       copy
+    resolution  1280x720
+
+Options:
+    -s | --startcam [#]         The video index number of the first camera (ex. 2 for /dev/video2)
+    -n | --numcam [#]           The number of cameras to record from.
+    -t | --time [#]             The duration of the recordings in seconds.
+    -c | --codec [copy]         The codec of the output video files
+    -r | --resolution [#x#]     The resolution of the cameras
 
--- record.sh
+++ record.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 clear
 
 # Get arguments
@@ -7,8 +6,16 @@ while [[ $# > 1 ]]
 do
 key="$1"
 case $key in
+    -s|--startcam)
+    STARTCAM="$2"
+    shift
+    ;;
     -n|--numcam)
     NUMCAM="$2"
+    shift
+    ;;
+    -r|--resolution)
+    RESOLUTION="$2"
     shift
     ;;
     -t|--time)
@@ -18,9 +25,9 @@ case $key in
     -c|--codec)
     CODEC="$2"
     shift
-    ;;
+    ;;    
     *)
-        # unknown option
+    # unknown option
     ;;
 esac
 shift # past argument or value
@@ -28,24 +35,38 @@ done
 
 
 # Set default parameters if not set.
+STARTCAM=${STARTCAM-0}
 NUMCAM=${NUMCAM-1}
-DURATION=${DURATION-20}
-CODEC=${CODEC-mjpeg}
+RESOLUTION=${RESOLUTION-1280x720}
+DURATION=${DURATION-15}
+CODEC=${CODEC-copy}
+
 
 # List the Cameras being used.
+echo -e "MultiCamRec: Multiple Camera Recorder"
 echo -e "Number of Cameras: $NUMCAM"
 echo -e "Duration: $DURATION"
 echo -e "Output Codec: $CODEC"
+echo -e "Resolution: $RESOLUTION"
+
 
 # Loop through each camera and launch the capture process.
 ((NUMCAM--))
-for i in $(seq 0 $NUMCAM)
+ENDCAM=$((STARTCAM+NUMCAM))
+for i in $(seq $STARTCAM $ENDCAM)
 do
 	echo "/dev/video$i"
-	avconv -f video4linux2 -input_format mjpeg -video_size 1280x720 -i /dev/video$i -c:v copy -t $DURATION -y cam_$i.mp4 2>&1 &> cam_$i.log &
+    avconv -f video4linux2 -input_format mjpeg -video_size $RESOLUTION -i /dev/video$i -c:v copy -t $DURATION -y cam_$i.mp4 2>&1 &> cam_$i.log &
 
+    # TODO: abstract the command, start log files with the command, and implement the h264 option.
+	#COMMAND="avconv -f video4linux2 -input_format mjpeg -video_size $RESOLUTION -i /dev/video$i -c:v copy -t $DURATION -y cam_$i.mp4 2>&1 &> cam_$i.log"
+    #echo $COMMAND
 	# Use h264 to shrink output filesize.
 	#avconv -f video4linux2 -input_format mjpeg -video_size 1280x720 -i /dev/video0 -vcodec libx264 -preset ultrafast -threads 0 -t 60 -y cam_0.mp4 2>&1 &> cam_0.log &
 done
 
-echo -e "RECORDINGS LAUNCHED.\n"
+
+# Output Process IDs
+ps | grep -v -E "(ps|grep|bash|multicamrec)"
+echo
+
